6ce5dec84bf82e2666dcd9ec4b1dc2173ef4b345
kafou135 <youssefkafou@gmail.com>
2025-03-22 02:06:38 +0000

    Update website

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -3,23 +3,41 @@ import Match from "./components/test";
 import getFixtureByFixtureId from "@/app/util/getFixtureByFixtureId";
 import getH2H from "@/app/util/getH2H";
 import getLineup from "@/app/util/getLineup";
-import { Events, Fixture, H2H,Lineups } from "@/types";
+import { Events, Fixture, H2H, Lineups } from "@/types";
+import { GetServerSidePropsContext } from "next";
 
+// Define the PageProps type for server-side props
 type PageProps = {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 };
 
-export default async function page({ params }: PageProps){
-    let fixtureByFixtureId: Fixture | undefined = await getFixtureByFixtureId(parseInt(params.id));
-    let h2h:H2H[] = await getH2H(Number(fixtureByFixtureId?.teams.home.id),Number(fixtureByFixtureId?.teams.away.id));
-    let lineups:Lineups[] = await getLineup(Number(fixtureByFixtureId?.fixture.id));
-    let events:Events[]= await getEvents(Number(fixtureByFixtureId?.fixture.id))
-    return (
-        <div>
-            <Match fixtureByFixtureId={fixtureByFixtureId} h2h={h2h} lineups={lineups} events={events}/>
-        </div>
-    )
+export default async function Page({ params }: PageProps) {
+  // Parse and fetch the fixture data
+  const fixtureByFixtureId: Fixture | undefined = await getFixtureByFixtureId(parseInt(params.id));
+  
+  // Fetch H2H data
+  const h2h: H2H[] = await getH2H(
+    Number(fixtureByFixtureId?.teams.home.id),
+    Number(fixtureByFixtureId?.teams.away.id)
+  );
+  
+  // Fetch lineups
+  const lineups: Lineups[] = await getLineup(Number(fixtureByFixtureId?.fixture.id));
+  
+  // Fetch events
+  const events: Events[] = await getEvents(Number(fixtureByFixtureId?.fixture.id));
 
+  return (
+    <div>
+      <Match fixtureByFixtureId={fixtureByFixtureId} h2h={h2h} lineups={lineups} events={events} />
+    </div>
+  );
+}
+
+// Add server-side props for dynamic page
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { id } = context.params;
+  return { props: { params: { id } } };
 }
