12187fbdf72afc5808e11a26d04ae63ac7a6a77b
kafou135 <youssefkafou@gmail.com>
2025-03-22 02:09:41 +0000

    Update website

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -4,9 +4,8 @@ import getFixtureByFixtureId from "@/app/util/getFixtureByFixtureId";
 import getH2H from "@/app/util/getH2H";
 import getLineup from "@/app/util/getLineup";
 import { Events, Fixture, H2H, Lineups } from "@/types";
-import { GetServerSidePropsContext } from "next";
 
-// Define the PageProps type for server-side props
+// Define the PageProps type for dynamic route params
 type PageProps = {
   params: {
     id: string;
@@ -14,19 +13,19 @@ type PageProps = {
 };
 
 export default async function Page({ params }: PageProps) {
-  // Parse and fetch the fixture data
   const fixtureByFixtureId: Fixture | undefined = await getFixtureByFixtureId(parseInt(params.id));
   
-  // Fetch H2H data
+  if (!fixtureByFixtureId) {
+    return <div>Fixture not found</div>;
+  }
+
   const h2h: H2H[] = await getH2H(
     Number(fixtureByFixtureId?.teams.home.id),
     Number(fixtureByFixtureId?.teams.away.id)
   );
   
-  // Fetch lineups
   const lineups: Lineups[] = await getLineup(Number(fixtureByFixtureId?.fixture.id));
   
-  // Fetch events
   const events: Events[] = await getEvents(Number(fixtureByFixtureId?.fixture.id));
 
   return (
@@ -34,10 +33,4 @@ export default async function Page({ params }: PageProps) {
       <Match fixtureByFixtureId={fixtureByFixtureId} h2h={h2h} lineups={lineups} events={events} />
     </div>
   );
-}
-
-// Add server-side props for dynamic page
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { id } = context.params;
-  return { props: { params: { id } } };
 }
