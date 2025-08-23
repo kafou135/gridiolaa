f3471dd046d080b18e235f1fd4d5f8a219f903a4
kafou135 <youssefkafou@gmail.com>
2025-08-24 00:28:43 +0100

    update website

--- gridiolaa/app/page.tsx
+++ gridiolaa/app/page.tsx
@@ -1,5 +1,4 @@
 import Hurrican from "./trends/hurricane-erin/page"
-import Home from "./components/FeedBack";
 export const metadata = {
   title: "Taylor Swift Drops New Album: The Life of a Showgirl",
   description: "Taylor Swift reveals her 12th studio album with a unique orange and mint-green theme, collaborations, and special physical editions."
@@ -9,6 +8,5 @@ export default function Page() {
   return( 
     <>
   <Hurrican/>
-  <Home/>
   </>);
 }
